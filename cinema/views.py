7a5697c603a5ee06d2b772ba04dc639d2e5181de
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-21 10:47:43 +0300

    views.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,23 @@
-# write views here
+  from rest_framework import viewsets
+  from .models import Genre, Actor, CinemaHall, Movie, MovieSession
+  from .serializers import GenreSerializer, ActorSerializer, CinemaHallSerializer, MovieSerializer, MovieSessionSerializer
+
+  class GenreViewSet(viewsets.ModelViewSet):
+      queryset = Genre.objects.all()
+      serializer_class = GenreSerializer
+
+  class ActorViewSet(viewsets.ModelViewSet):
+      queryset = Actor.objects.all()
+      serializer_class = ActorSerializer
+
+  class CinemaHallViewSet(viewsets.ModelViewSet):
+      queryset = CinemaHall.objects.all()
+      serializer_class = CinemaHallSerializer
+
+  class MovieViewSet(viewsets.ModelViewSet):
+      queryset = Movie.objects.prefetch_related('genres', 'actors')
+      serializer_class = MovieSerializer
+
+  class MovieSessionViewSet(viewsets.ModelViewSet):
+      queryset = MovieSession.objects.select_related('movie', 'cinema_hall')
+      serializer_class = MovieSessionSerializer
